67a9363026b6f4c591eec79741051bbe6dbfee85
EvanOrunto <evanyinkaorunto@yahoo.com>
2022-04-23 09:38:07 +0100

    Prime number checker

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lab3ass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int number;
+            Console.Write("Input a number:");
+            number = Convert.ToInt32(Console.ReadLine());
+
+           switch(number)
+           {
+               case 17:
+               Console.WriteLine("Prime number");
+               break;
+
+               case 57:
+               Console.WriteLine("Not prime number");
+               break;
+           }
+        }
+    }
+}
